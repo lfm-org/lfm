715ee1378d863f7cd9cda2761c032991b0a7f62e
Sour Old Geezer <souroldgeezer@proton.me>
2020-03-04 19:17:07 +0200

    No idea

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,9 @@
 import { NestFactory } from "@nestjs/core";
-import { NestExpressApplication } from "@nestjs/platform-express";
-import { join } from "path";
 import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: true
-  });
-  app.useStaticAssets(join(__dirname, "..", "public"));
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
