02e9d5eec66431eef7a5a479ddb01c0bd80e3f44
Sour Old Geezer <souroldgeezer@proton.me>
2026-03-22 22:23:16 +0200

    Remove subtitle from logo

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,71 +1,18 @@
-import React from "react";
-import { Box, Link, Typography } from "@mui/material";
+import { Link, Typography } from "@mui/material";
 import { Link as RouterLink } from "react-router";
 
-export interface ILogoProps {
-  title: string;
-  subtitles?: string[];
-}
-
-export const DEFAULT_SUBTITLES = [
-  "What's our tank doing?!",
-  "Pre-potion usage f(x) = 1/x",
-  "How long has that Rogue been offline?",
-  "Let's Twitch again, like we did last summer",
-];
-
-interface ILogoState {
-  subtitle: string;
-}
-
-export class Logo extends React.Component<ILogoProps, ILogoState> {
-  public static defaultProps = {
-    subtitles: DEFAULT_SUBTITLES,
-  };
-
-  constructor(props: ILogoProps) {
-    super(props);
-    this.state = { subtitle: (props.subtitles ?? DEFAULT_SUBTITLES)[0] };
-  }
-
-  public componentDidMount() {
-    const subtitles = this.props.subtitles ?? DEFAULT_SUBTITLES;
-    this.setState({
-      subtitle: subtitles[Math.floor(Math.random() * subtitles.length)],
-    });
-  }
-
-  public render() {
-    return (
-      <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1, minWidth: 0 }}>
-        <Link
-          component={RouterLink}
-          to="/"
-          underline="none"
-          color="inherit"
-          sx={{ display: "inline-flex", alignItems: "center", flexShrink: 0 }}
-        >
-          <Typography
-            variant="h6"
-            sx={{ mr: { xs: 2, md: 4 }, whiteSpace: "nowrap" }}
-          >
-            {this.props.title}
-          </Typography>
-        </Link>
-        <Typography
-          variant="subtitle2"
-          data-testid="logo-subtitle"
-          sx={{
-            minWidth: 0,
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            whiteSpace: "nowrap",
-            color: "text.secondary",
-          }}
-        >
-          {this.state.subtitle}
-        </Typography>
-      </Box>
-    );
-  }
+export default function Logo({ title }: { title: string }) {
+  return (
+    <Link
+      component={RouterLink}
+      to="/"
+      underline="none"
+      color="inherit"
+      sx={{ flexGrow: 1, display: "inline-flex", alignItems: "center", flexShrink: 0 }}
+    >
+      <Typography variant="h6" sx={{ mr: { xs: 2, md: 4 }, whiteSpace: "nowrap" }}>
+        {title}
+      </Typography>
+    </Link>
+  );
 }
