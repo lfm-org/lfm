da14cf6af2c842f67c26724d17aba81cf4a12f0e
Sour Old Geezer <souroldgeezer@proton.me>
2026-04-06 00:43:44 +0300

    Add failing InstancesListFunction test

---
+++ tests/Lfm.Api.Tests/InstancesListFunctionTests.cs
@@ -0,0 +1,32 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Lfm.Api.Functions;
+using Lfm.Api.Repositories;
+using Xunit;
+
+namespace Lfm.Api.Tests;
+
+public class InstancesListFunctionTests
+{
+    [Fact]
+    public async Task Returns_instances_from_repository()
+    {
+        var repo = new Mock<IInstancesRepository>();
+        repo.Setup(r => r.ListAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new List<InstanceRecord>
+            {
+                new("liberation-of-undermine", "Liberation of Undermine", "raid", "tww"),
+                new("manaforge-omega", "Manaforge Omega", "raid", "tww")
+            });
+        var fn = new InstancesListFunction(repo.Object);
+
+        var result = await fn.Run(new DefaultHttpContext().Request, CancellationToken.None);
+
+        var ok = result.Should().BeOfType<OkObjectResult>().Subject;
+        var items = ok.Value.Should().BeAssignableTo<IReadOnlyList<InstanceRecord>>().Subject;
+        items.Should().HaveCount(2);
+        items[0].Id.Should().Be("liberation-of-undermine");
+    }
+}
