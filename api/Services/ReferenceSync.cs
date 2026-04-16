3a166987300caa562ad53cf0f4b71d6aa6984492
Sour Old Geezer <souroldgeezer@proton.me>
2026-04-16 19:09:14 +0300

    Add SPDX headers to api/ (batch phase-2a-batch-ap)

--- api/Services/ReferenceSync.cs
+++ api/Services/ReferenceSync.cs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-FileCopyrightText: 2026 LFM contributors
+
 using Lfm.Api.Repositories;
 using Lfm.Contracts.Admin;
 using Microsoft.Extensions.Logging;
