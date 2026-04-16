e470d29e36fa781ebd6defedcee5bedeacc008b4
Sour Old Geezer <souroldgeezer@proton.me>
2026-04-16 20:41:38 +0300

    Add SPDX headers to app/ (batch phase-2b-batch-ad)

--- app/Program.cs
+++ app/Program.cs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-FileCopyrightText: 2026 LFM contributors
+
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
